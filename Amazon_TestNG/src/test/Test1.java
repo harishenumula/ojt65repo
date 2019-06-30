93794c5774c00e16b838e0abb248cda49f88f7d7
Harish <harishenumula5@gmail.com>
2019-06-30 12:07:51 +0530

    adding test1 method

--- Amazon_TestNG/src/test/Test1.java
+++ Amazon_TestNG/src/test/Test1.java
@@ -3,5 +3,7 @@ package test;
 public class Test1 {
 	public void start() {
 		System.out.println("Welcome to Test File");
+	}public void start1() {
+		System.out.println("Welcome to Test File");
 	}
 }
